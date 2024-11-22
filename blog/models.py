65e49d47cd88c11d6aa0a13c2d686f41ba0204f1
I-of-Horus <han1968@gmail.com>
2024-11-22 13:18:55 +0100

    changed blog to footballer db

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,21 @@ from django.db import models
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+"""
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+"""
+class Post_footballer(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    footballer_name = models.CharField(max_length=100, default="default name")
+    footballer_curr_club  = models.CharField(max_length=100, default="default clubname")
+  
+    created_date = models.DateTimeField(default=timezone.now)
+    last_mod_date = models.DateTimeField(blank=True, null=True)
     published_date = models.DateTimeField(blank=True, null=True)
 
 
